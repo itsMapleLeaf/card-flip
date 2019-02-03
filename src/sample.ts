a4318aad6a3fa6bf08d83cee875d4e238ef5d333
kingdaro <kingdaro@gmail.com>
2019-02-03 15:46:49 -0600

    sample: minor optimization + doc comment

--- src/sample.ts
+++ src/sample.ts
@@ -1,13 +1,19 @@
+/**
+ * Returns a number of items randomly from the array
+ */
 const sample = <T>(items: T[], count: number) => {
   const copied = [...items]
-  const result: T[] = []
+  const result = Array<T>(count)
+
   for (let i = 0; i < count; i++) {
     const [sampledItem] = copied.splice(
       Math.floor(Math.random() * copied.length),
       1,
     )
-    result.push(sampledItem)
+
+    result[i] = sampledItem
   }
+
   return result
 }
 export default sample
